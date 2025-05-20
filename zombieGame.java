bc98a8703f5d109910578b24956663cec621ac92
jonowpRMIT <S4151053@student.rmit.edu.au>
2025-05-20 12:23:35 +1000

    Add files via upload

--- zombieGame.java
+++ zombieGame.java
@@ -6,37 +6,50 @@ public class zombieGame {
         int[] playerPosition = {0, 0};
         boolean inCombat = false;
 
-        do {
+        if (inCombat == true) {
+            System.out.println("work");
+        }
+
+        while (inCombat == false) {
             System.out.println("use WASD to move");
             System.out.println("you are in room " + playerPosition[0] + ", " + playerPosition[1]);
             String playerMoveInput = playerMoveInputScan.nextLine().toUpperCase();
-
+    
                 switch (playerMoveInput) {
                     case "W":
                         playerPosition[0] = playerPosition[0] + 1;
                         break;
-                
+                    
                     case "A":
                     playerPosition[1] = playerPosition[1] + 1;
                         break;
-                
+                    
                     case "S":
                     playerPosition[0] = playerPosition[0] - 1;
                         break;
-                    
+                        
                     case "D":
                     playerPosition[1] = playerPosition[1] - 1;
                         break;
-                
+                    
                     default: System.out.println("invalid input");
                         break;
                 }
-                if (playerPosition[0] >= 6 || playerPosition[0] <= -1 || playerPosition[1] >= 6 || playerPosition[1] <= -1) {
-                    System.out.println("out of bounds");
-                    playerPosition[0] = 0;
-                    playerPosition[1] = 0;
-                    }
-            
-        } while (inCombat == false);
+
+            if (playerPosition[0] >= 6 || playerPosition[0] <= -1 || playerPosition[1] >= 6 || playerPosition[1] <= -1) {
+                System.out.println("out of bounds");
+                playerPosition[0] = 0;
+                playerPosition[1] = 0;
+                }
+
+            if (playerPosition[0] == 2) {
+                System.out.println("combatt");
+                inCombat = true;
+            }
+        }
+
+        if (inCombat == true) {
+            System.out.println("work");
+        }
     }
 }
